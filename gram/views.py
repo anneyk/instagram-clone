34aa615b0e22f7357f1e82ee68e85a06cb08e799
Annetkhavere <anneywan5@gmail.com>
2022-06-06 01:04:20 +0300

    update the login and index views

--- gram/views.py
+++ gram/views.py
@@ -1,45 +1,47 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render,redirect
-from .forms import UserRegistrationForm
+
+from gram.models import Post
+from .forms import PostForm, SignUpForm, UserRegistrationForm
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .forms import UpdateUserForm,UpdateUserProfileForm
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.models import User
 # Create your views here.
-def index(request):
-  return render(request, 'index.html')
 
 def register(request):
-	if request.method == "POST":
-		form = UserRegistrationForm(request.POST)
-		if form.is_valid():
-			form.save()
-			# user = form.save()
-			# login(request, user)
-			messages.success(request, f"Registration successful." )
-			return redirect("login")
-	else:
-		form = UserRegistrationForm()
-
-	context = {'form':form}
-	return render (request, 'register.html', context)
+  if request.method == 'POST':
+    form = SignUpForm(request.POST)
+    if form.is_valid():
+      form.save()
+      username = form.cleaned_data.get('username')
+      raw_password = form.cleaned_data.get('password1')
+      user = authenticate(username=username, password=raw_password)
+      login(request, user)
+      return redirect('index')
+    else:
+      form = SignUpForm()
+    return render(request, 'register.html', {'form': form})
 
 @login_required(login_url='login')
-def profile(request,username):
-  images = request.user.profile.posts.all()
-  if request.method == 'POST':
-    user_form = UpdateUserForm(request.POST,instance=request.user)
-    prof_form = UpdateUserProfileForm(request.POST, request.FILES, instance=request.user.profile)
-    if user_form.is_valid() and prof_form.is_valid():
-      user_form.save()
-      prof_form.save()
-      return HttpResponseRedirect(request.path_info)
+def index(request):
+    images = Post.objects.all()
+    users = User.objects.exclude(id=request.user.id)
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = request.user.profile
+            post.save()
+            return HttpResponseRedirect(request.path_info)
     else:
-      user_form = UpdateUserForm(instance=request.user)
-      prof_form = UpdateUserProfileForm(instance=request.user.profile)
-      params = {
-        'user_form': user_form,
-        'prof_form': prof_form,
+        form = PostForm()
+    params = {
         'images': images,
-      }
-      return render(request,'gram/profile.html', params)
-	
+        'form': form,
+        'users': users,
+
+    }
+    return render(request, 'index.html', params)
+
