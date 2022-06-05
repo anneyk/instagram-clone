f91b590a04e756fdf6c1faa6a54eb0a107936d3c
Annetkhavere <anneywan5@gmail.com>
2022-06-05 17:18:21 +0300

    add the signup,login and logout

--- gram/models.py
+++ gram/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class NewUser(models.Model):
+    OUR_CHOICES =(
+    ("Mgr", "Manager"),
+    ("Emp", "Employee"),
+    ("Admin", "Administrator"),
+    ("Sup", "Supervisor"),
+    )
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    email = models.EmailField()
+    password1 = models.CharField(max_length=30, blank=True)
+    password2 = models.CharField(max_length=30)
--- gram/views.py
+++ gram/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from .forms import UserRegistrationForm
+from django.contrib import messages
 # Create your views here.
+def index(request):
+  return render(request, 'index.html')
+
+def register(request):
+	if request.method == "POST":
+		form = UserRegistrationForm(request.POST)
+		if form.is_valid():
+			form.save()
+			# user = form.save()
+			# login(request, user)
+			messages.success(request, f"Registration successful." )
+			return redirect("login")
+	else:
+		form = UserRegistrationForm()
+
+	context = {'form':form}
+	return render (request, 'register.html', context)
+	
